0ee0140d3740f561aceb2300a8edf97287cb4105
takusankai <j021328@gmail.com>
2024-01-21 01:25:58 +0900

    pykakasi導入とgptの出力から選択肢1のデータと選択肢2のデータを探してくる。

--- GPT.py
+++ GPT.py
@@ -3,6 +3,18 @@ from flask_cors import CORS
 import os
 from openai import OpenAI
 import json
+from pykakasi import kakasi
+
+kakasi = kakasi()
+# 漢字⇒訓令式アルファベット変換を設定
+kakasi.setMode("J", "a")
+# ひらがな⇒訓令式アルファベット変換を設定
+kakasi.setMode("H", "a")
+conv = kakasi.getConverter()
+
+# デバッグ用
+# text = "じゃあちゃんと注意しゅるふぉ"
+# print(conv.do(text))
 
 # Flaskの起動
 app = Flask(__name__)
@@ -15,16 +27,20 @@ def home():
     return generate()
 
 def generate():
+
     data = request.get_json()
+
     # デバッグ用
-    print(data)
-    print(data.get('key'))
+    # print(data)
+    # print(data.get('key'))
     
     # APIクライアントの初期化
     client = OpenAI(
+
         # This is the default and can be omitted
         organization=os.environ.get('OpenAI_organization'),
         api_key=os.environ.get('API_KEY_openai')
+
     )
 
     #messages = data.get('messages', [])
@@ -45,16 +61,24 @@ def generate():
         top_p=1,  # トークン選択時の確率閾値
     )
 
-    # 生成されたテキストの取得
-    print(response)
-    for i, choice in enumerate(response.choices):
-        print(f"\nresult {i}:")
-        print(choice.message.content.strip())
-    #修正前
-    #return jsonify([choice['message']['content'] for choice in response['choices']])
-    #修正後(rawjson)
-    return jsonify([choice.message.content for choice in response.choices])
-        
+    # 生成されたテキストが複数だった時の取得
+    # ながーい1プロンプトで処理するならforループは不要
+    # for i, choice in enumerate(response.choices):
+        # print(f"\nresult {i}:")
+        # print(choice.message.content.strip())
+    choice = response.choices[0]
+
+    # json.loads関数を使ってPythonデータ構造のJSON形式への変換
+    json_data = json.loads(choice.message.content.strip())
+    
+    # デバッグ用
+    print(json_data['選択肢1'])
+    print(json_data['選択肢2'])
+
+    # ながーい1プロンプトで処理するならforループは不要
+    # return jsonify([choice.message.content for choice in response.choices])
+    return jsonify(choice.message.content.strip())
+
     # 日本語出力
     # 生成されたテキストの取得
     # result = [choice.message.content for choice in response.choices]
@@ -64,5 +88,6 @@ def generate():
     # JSON形式での返却（日本語をそのまま出力）
     # return make_response(json.dumps(result, ensure_ascii=False))
 
+# 初回の起動処理
 if __name__ == '__main__':
     app.run(debug=True)
